0ff33f81f83e2ce3216910ece357d7c1e91cadf1
CarlosKim <dlsxody1@gmail.com>
2022-09-01 15:05:52 +0900

    질문하기 전 커밋

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const cors = require("cors");
+app.use(cors);
+dotenv.config();
+
 var db;
+let userId = {};
+
 MongoClient.connect(
   `mongodb+srv://${process.env.MONGODB_KEY}@cluster0.hl7ifoa.mongodb.net/?retryWrites=true&w=majority`,
   (err, client) => {
-    if (err) return console.log(err);
+    if (err) return console.log(process.env.MONGODB_KEY);
     db = client.db("bacharta");
-    app.listen(3000, () => {
-      console.log("d1d");
-      console.log(process.env.MONGODB_KEY);
+    app.listen(3001, () => {
+      console.log("서버가동");
     });
   }
 );
 
-app.get("/app", (req, res) => {
-  db.collection("bacharta").insertOne({ kakaoPk: 20 }, () => {
+let token = {};
+app.post(`http://127.0.0.1:3001/user/sign`, async (req, res) => {
+  await axios
+    .post("https://kapi.kakao.com/v2/user/me", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((userId = res.data))
+    .then(console.log(userId))
+    .then();
+  const findUser = db.collection("bacharta").findOne({ kakaoPk: userId });
+  if (!findUser) return userId;
+  db.collection("bacharta").insertOne({ kakaoPk: userId }, () => {
     console.log("저장완료");
+
+    return res
+      .header("Authorization", token)
+      .status(200)
+      .json({ message: "Success" });
   });
+
+  //const kakaoToken = 1;
+  //const token = jwt.sign({});
 });
