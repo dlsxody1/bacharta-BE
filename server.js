1bb4e80a8c0ff98b1169022cef96662cecf4a774
김인태 <dlsxody1@naver.com>
2022-10-07 19:57:21 +0900

    feat : 범죄 통계 차트를 위한 api 구현

--- server.js
+++ server.js
@@ -39,7 +39,21 @@ const fullDay = `${date.getFullYear()}${
 
 const exchangeAPI = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.EXCHANGE_KEY}&searchdate=${fullDay}&data=AP01`;
 const atmosphereAPI = `http://apis.data.go.kr/1480523/MetalMeasuringResultService/MetalService?numOfRows=1&pageNo=1&resultType=xml&stationcode=1&date=${fullDay}&timecode=RH02&itemcode=90303&serviceKey=${process.env.ATMOSPHERE_KEY}`;
+const covidAPI = `http://apis.data.go.kr/1790387/covid19CurrentStatusHospitalizations/covid19CurrentStatusHospitalizationsJson?serviceKey=${process.env.COVID_KEY}`;
+const crimeAPI = "http://api.sexoffender.go.kr/openapi/SOCitysStats/";
 
+app.get("/crime", async (req, res) => {
+  const getData = await axios.get(crimeAPI, (err, res, body) => {
+    const result = body;
+    const xmlToJson = convert.xml2js(result, {
+      compact: true,
+      spaces: 4,
+    });
+    return xmlToJson;
+  });
+  const xmlToJson = convert.xml2js(getData.data, { compact: true, spaces: 4 });
+  return res.status(200).json({ data: xmlToJson });
+});
 app.post("/select-outfit", (req, res) => {
   // ** Todo -> DB에 저장하고 불러올지 고민해봐야함 요청했을 때 테스트도 해봐야함.
   // DB에 있는 데이터를 꺼내서 프론트에서 일치하는지 확인해 봐야할 수 도 있음.
@@ -47,6 +61,15 @@ app.post("/select-outfit", (req, res) => {
   // db.collection("outfit").insertOne(req);
 
   return res.status(200).json({ message: "이후경 화이팅", data: req });
+});
+
+app.get("/covid", async (req, res) => {
+  const getData = await axios.get(covidAPI);
+
+  return res.status(200).json({
+    message: "covid data 통신 성공",
+    data: getData.data.response.result,
+  });
 });
 
 app.get("/getoutfit/:userToken", (req, res) => {
@@ -66,7 +89,7 @@ app.get("/exchange", async (req, res) => {
   console.log("exchage 겟요청");
   let exchageData = {};
   const getData = await axios.get(exchangeAPI);
-  console.log(getData);
+
   res.status(200).json({ message: "성공", data: getData.data });
 });
 
