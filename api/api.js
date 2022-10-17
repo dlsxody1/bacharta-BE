e9506cc58c2e07b3909bc01fc1cda515d1a06967
김인태 <dlsxody1@naver.com>
2022-10-17 20:51:50 +0900

    refactor : location Data 에서 지역만 주는 것으로 수정

--- api/api.js
+++ api/api.js
@@ -7,7 +7,8 @@ const fullDay = `${date.getFullYear()}${
 }${date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()}`;
 
 const exchangeAPI = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${process.env.EXCHANGE_KEY}&searchdate=${fullDay}&data=AP01`;
+const atmosphereAPI = `http://apis.data.go.kr/1480523/MetalMeasuringResultService/MetalService?numOfRows=1&pageNo=1&resultType=xml&stationcode=1&date=${fullDay}&timecode=RH02&itemcode=90303&serviceKey=${process.env.ATMOSPHERE_KEY}`;
 const covidAPI = `http://apis.data.go.kr/1790387/covid19CurrentStatusHospitalizations/covid19CurrentStatusHospitalizationsJson?serviceKey=${process.env.COVID_KEY}`;
 const crimeAPI = "http://api.sexoffender.go.kr/openapi/SOCitysStats/";
 
-module.exports = { exchangeAPI, covidAPI, crimeAPI };
+module.exports = { exchangeAPI, atmosphereAPI, covidAPI, crimeAPI };
--- routes/locationRouter.js
+++ routes/locationRouter.js
@@ -7,14 +7,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-router.get("/", async (req, res) => {
+router.get("", async (req, res) => {
+  console.log(locationData);
   const getData = await Promise.all(
     locationData.map(async ({ lat, lng }) => {
       const request = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${process.env.OPEN_WEATHER_KEY}`
       );
 
-      return request.data;
+      return request.data.name;
     })
   );
   console.log(getData);
